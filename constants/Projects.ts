c419de0093dc2ddb00e1f1dc24bee07ac57e1fcb
JADundee <jadundee@icloud.com>
2025-09-18 02:45:50 -0600

    Updated Projects

--- constants/Projects.ts
+++ constants/Projects.ts
@@ -2,30 +2,37 @@
 export const PROJECTS = [
   {
     id: 1,
-    title: "Project One",
-    description: "Brief description of your first project and its purpose.",
-    techStack: "React Native, TypeScript",
-    link: "https://github.com/your-username/project-one", // or live demo URL
+    title: "Forum Showcase",
+    description: "Sign up or use, Username: Test, Password: !123 to explore the features. A MERN stack forum application with user authentication + Authorization, Custom API endpoints, CRUD operations, mongoDB database, and real-time updates.",
+    techStack: "MERN (MongoDB, Express, React, Node.js), JWT, CSS, Redux, Moment.js, CORS, Mongoose, bcrypt",
+    link: "https://forum-ahrf.onrender.com", // or live demo URL
   },
   {
     id: 2,
-    title: "Project Two",
-    description: "Short summary of what this project does and why it’s cool.",
-    techStack: "Next.js, Tailwind CSS",
-    link: "https://your-portfolio.com/project-two",
+    title: "Warcraft Name Generator",
+    description: "Classic JavaScript project that generates random Warcraft character names with over 50,000 combinations and API to check availabilty of selected names on a selected WoW realm",
+    techStack: "JavaScript, HTML, CSS, REST API, Netlify Functions",
+    link: "https://majestic-parfait-596444.netlify.app/",
   },
   {
     id: 3,
-    title: "Project Three",
-    description: "Another example project. Highlight any unique features.",
-    techStack: "Node.js, Express, MongoDB",
-    link: "https://github.com/your-username/project-three",
+    title: "Github",
+    description: "Link to my GitHub profile containing various coding concepts and contributions.",
+    techStack: "",
+    link: "https://github.com/JADundee",
   },
-  {
+  /* {
     id: 4,
-    title: "Project Four",
-    description: "Optional: include a non-coding project or case study.",
-    techStack: "Python, Flask, PostgreSQL",
+    title: "LinkedIn",
+    description: "",
+    techStack: "",
     link: "", // leave blank if no link yet
-  },
+  }, */ 
+  {
+    id: 5,
+    title: "Resume",
+    description: "PDF version of my resume/CV.",
+    techStack: "",
+    link: "https://www.dropbox.com/scl/fi/krfzmv18xj2fds30eowxk/CV3.pdf?rlkey=oufy7w0amf3syg3pzqmmefk2j&st=ppyjj2wo&dl=0", // leave blank if no link yet
+  }, 
 ];
